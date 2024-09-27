5d014a1a367c3bc0b448fb6999b89d493580ddc9
kKryst <92850700+kKryst@users.noreply.github.com>
2024-09-27 10:42:59 +0200

    Initial Commit

---
+++ FamilyPlaytime/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FamilyPlaytime
+//
+//  Created by Krystian Konieczko on 27/09/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FamilyPlaytime/FamilyPlaytimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FamilyPlaytimeApp.swift
+//  FamilyPlaytime
+//
+//  Created by Krystian Konieczko on 27/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct FamilyPlaytimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
